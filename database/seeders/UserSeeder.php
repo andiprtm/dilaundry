5ebd60e69a9790816af5d3b196ec180fa74c8d03
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-07-22 20:46:40 +0700

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+          [
+              'username' => 'admin',
+              'email' => 'admin@gmail.com',
+              'password' => Hash::make('admin123'),
+          ]
+        ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
